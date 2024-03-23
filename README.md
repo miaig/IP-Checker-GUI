97844eda55b29e136ff483dc011a29adf60763f8
timtom2016 <dev@timtom2016.com>
2024-03-23 17:49:16 +0100

    Small Fixes and Added README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# IP-Checker-GUI
+IP Checker GUI is a simple tool for teachers that can make their students input the corresponding Network Address, Subnet Mask, and more to test their skill.
+
+## Installation
+1. Clone the repository
+```bash
+git clone https://github.com/TimTom2016/IP-Checker-GUI.git
+```
+2. Install gtk4 for
+
+   https://gtk-rs.org/gtk4-rs/stable/latest/book/installation.html
+
+3.  Install Rust
+```bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+4. Run the program
+```bash
+cargo run --release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,67 @@
-use gtk4::InputHints;
-use gtk4::prelude::{EditableExt, EntryExt, WidgetExt};
+use gtk4::prelude::{EditableExt, EntryExt};
 use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
-use relm4::{gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
 use rand::{Rng, thread_rng};
-use std::borrow::BorrowMut;
+use relm4::{ComponentParts, ComponentSender, gtk, RelmApp, RelmWidgetExt, SimpleComponent};
 use relm4_icons::icon_names;
+
 struct AppModel {
-    ip: Option<IpModel>,
-    user_input: IpModel,
-    valid: Validator,
+	ip: Option<IpModel>,
+	user_input: IpModel,
+	valid: Validator,
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 enum EntryInput {
-    Mask(String),
-    NetworkAddress(String),
-    BroadcastAddress(String),
-    FirstHost(String),
-    LastHost(String),
-    PossibleHosts(u32),
+	Mask(String),
+	NetworkAddress(String),
+	BroadcastAddress(String),
+	FirstHost(String),
+	LastHost(String),
+	PossibleHosts(u32),
 }
 
-#[derive(Debug,Default,Clone)]
+#[derive(Debug, Default, Clone)]
 struct IpModel {
-    ip: String,
-    subnet_mask: u8,
-    prefix: String,
-    mask: String,
-    binary_address: String,
-    network_address: String,
-    broadcast_address: String,
-    first_host: String,
-    last_host: String,
-    possible_hosts: u32,
+	ip: String,
+	subnet_mask: u8,
+	prefix: String,
+	mask: String,
+	binary_address: String,
+	network_address: String,
+	broadcast_address: String,
+	first_host: String,
+	last_host: String,
+	possible_hosts: u32,
 }
 
-#[derive(Debug,Default)]
+#[derive(Debug, Default)]
 struct Validator {
-    mask: bool,
-    network_address: bool,
-    broadcast_address: bool,
-    first_host: bool,
-    last_host: bool,
-    possible_hosts: bool,
+	mask: bool,
+	network_address: bool,
+	broadcast_address: bool,
+	first_host: bool,
+	last_host: bool,
+	possible_hosts: bool,
 }
-
-
-
 
 
 #[derive(Debug)]
 enum AppMsg {
-    GenerateIp,
-    CheckIp,
-    EntryInput(EntryInput),
+	GenerateIp,
+	CheckIp,
+	EntryInput(EntryInput),
 }
 
 #[relm4::component]
 impl SimpleComponent for AppModel {
-    type Input = AppMsg;
+	type Input = AppMsg;
 
-    type Output = ();
-    type Init = Option<IpModel>;
+	type Output = ();
+	type Init = Option<IpModel>;
 
-    view! {
+	view! {
         gtk::Window {
-            set_title: Some("Ip Calculater"),
+            set_title: Some("IP Checker"),
             set_default_width: 300,
             set_default_height: 100,
 
@@ -93,11 +89,9 @@ impl SimpleComponent for AppModel {
                     set_secondary_icon_name: if model.valid.mask { Some(icon_names::CHECK_PLAIN) } else { Some(icon_names::CROSS) },
                     set_margin_all: 5,
                     connect_changed[sender] => move |entry| {
-                        if let text = entry.text() {
-                            if let subnet_mask = text.to_string() {
-                                sender.input(AppMsg::EntryInput(EntryInput::Mask(subnet_mask)));
-                            }
-                        }
+                        let text = entry.text();
+                        let subnet_mask = text.to_string();
+                        sender.input(AppMsg::EntryInput(EntryInput::Mask(subnet_mask)));
                     },
                 },
                 gtk::Entry {
@@ -106,11 +100,9 @@ impl SimpleComponent for AppModel {
                     set_secondary_icon_name: if model.valid.network_address { Some(icon_names::CHECK_PLAIN) } else { Some(icon_names::CROSS) },
                     set_margin_all: 5,
                     connect_changed[sender] => move |entry| {
-                        if let text = entry.text() {
-                            if let ip = text.to_string() {
-                                sender.input(AppMsg::EntryInput(EntryInput::NetworkAddress(ip)));
-                            }
-                        }
+                        let text = entry.text();
+                        let ip = text.to_string();
+                        sender.input(AppMsg::EntryInput(EntryInput::NetworkAddress(ip)));
                     },
                 },
                 gtk::Entry {
@@ -119,11 +111,9 @@ impl SimpleComponent for AppModel {
                     set_secondary_icon_name: if model.valid.broadcast_address { Some(icon_names::CHECK_PLAIN) } else { Some(icon_names::CROSS) },
                     set_margin_all: 5,
                     connect_changed[sender] => move |entry| {
-                        if let text = entry.text() {
-                            if let broadcast = text.to_string() {
-                                sender.input(AppMsg::EntryInput(EntryInput::BroadcastAddress(broadcast)));
-                            }
-                        }
+                        let text = entry.text();
+                        let broadcast = text.to_string();
+                        sender.input(AppMsg::EntryInput(EntryInput::BroadcastAddress(broadcast)));
                     },
                 },
                 gtk::Entry {
@@ -132,11 +122,9 @@ impl SimpleComponent for AppModel {
                     set_secondary_icon_name: if model.valid.first_host { Some(icon_names::CHECK_PLAIN) } else { Some(icon_names::CROSS) },
                     set_margin_all: 5,
                     connect_changed[sender] => move |entry| {
-                        if let text = entry.text() {
-                            if let first = text.to_string() {
-                                sender.input(AppMsg::EntryInput(EntryInput::FirstHost(first)));
-                            }
-                        }
+                        let text = entry.text();
+                        let first = text.to_string();
+                        sender.input(AppMsg::EntryInput(EntryInput::FirstHost(first)));
                     },
                 },
                 gtk::Entry {
@@ -145,11 +133,9 @@ impl SimpleComponent for AppModel {
                     set_secondary_icon_name: if model.valid.last_host { Some(icon_names::CHECK_PLAIN) } else { Some(icon_names::CROSS) },
                     set_margin_all: 5,
                     connect_changed[sender] => move |entry| {
-                        if let text = entry.text() {
-                            if let last = text.to_string() {
-                                sender.input(AppMsg::EntryInput(EntryInput::LastHost(last)));
-                            }
-                        }
+                        let text = entry.text();
+                        let last = text.to_string();
+                        sender.input(AppMsg::EntryInput(EntryInput::LastHost(last)));
                     },
                 },
                 gtk::Entry {
@@ -158,11 +144,9 @@ impl SimpleComponent for AppModel {
                     set_secondary_icon_name: if model.valid.possible_hosts { Some(icon_names::CHECK_PLAIN) } else { Some(icon_names::CROSS) },
                     set_margin_all: 5,
                     connect_changed[sender] => move |entry| {
-                        if let text = entry.text() {
-                            if let possible = text.to_string() {
-                                sender.input(AppMsg::EntryInput(EntryInput::PossibleHosts(possible.parse().unwrap())));
-                            }
-                        }
+                        let text = entry.text();
+                        let possible = text.to_string();
+                        sender.input(AppMsg::EntryInput(EntryInput::PossibleHosts(possible.parse().unwrap())));
                     },
                 },
                 gtk::Button {
@@ -175,175 +159,172 @@ impl SimpleComponent for AppModel {
         }
     }
 
-    // Initialize the UI.
-    fn init(
-        ip: Self::Init,
-        root: Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let mut model = AppModel {ip: ip, user_input: IpModel::default(),valid: Validator::default()};
+	// Initialize the UI.
+	fn init(
+		ip: Self::Init,
+		root: Self::Root,
+		sender: ComponentSender<Self>,
+	) -> ComponentParts<Self> {
+		let model = AppModel { ip, user_input: IpModel::default(), valid: Validator::default() };
 
-        // Insert the macro code generation here
-        let widgets = view_output!();
+		// Insert the macro code generation here
+		let widgets = view_output!();
 
-        ComponentParts { model, widgets }
-    }
+		ComponentParts { model, widgets }
+	}
 
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
-        match msg {
-            AppMsg::GenerateIp => {
-                self.ip = Some(ip_calculations());
-            }
-            AppMsg::EntryInput(input) => {
-                match input {
-                    EntryInput::Mask(mask) => {
-                        self.user_input.mask = mask;
-                    }
-                    EntryInput::NetworkAddress(ip) => {
-                        self.user_input.ip = ip;
-                    }
-                    EntryInput::BroadcastAddress(broadcast) => {
-                        self.user_input.broadcast_address = broadcast;
-                    }
-                    EntryInput::FirstHost(first) => {
-                        self.user_input.first_host = first;
-                    }
-                    EntryInput::LastHost(last) => {
-                        self.user_input.last_host = last;
-                    }
-                    EntryInput::PossibleHosts(possible) => {
-                        self.user_input.possible_hosts = possible;
-                    }
-                }
-            }
-            AppMsg::CheckIp => {
-                self.valid = Validator {
-                    mask: self.ip.as_ref().map(|ip| &ip.mask).unwrap_or(&"".to_string()) == &self.user_input.mask,
-                    network_address: self.ip.as_ref().map(|ip| &ip.network_address).unwrap_or(&"".to_string()) == &self.user_input.ip,
-                    broadcast_address: self.ip.as_ref().map(|ip| &ip.broadcast_address).unwrap_or(&"".to_string()) == &self.user_input.broadcast_address,
-                    first_host: self.ip.as_ref().map(|ip| &ip.first_host).unwrap_or(&"".to_string()) == &self.user_input.first_host,
-                    last_host: self.ip.as_ref().map(|ip| &ip.last_host).unwrap_or(&"".to_string()) == &self.user_input.last_host,
-                    possible_hosts: self.ip.as_ref().map(|ip| &ip.possible_hosts).unwrap_or(&0) == &self.user_input.possible_hosts,
-                };
-            }
-        }
-    }
+	fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+		match msg {
+			AppMsg::GenerateIp => {
+				self.ip = Some(ip_calculations());
+			}
+			AppMsg::EntryInput(input) => {
+				match input {
+					EntryInput::Mask(mask) => {
+						self.user_input.mask = mask;
+					}
+					EntryInput::NetworkAddress(ip) => {
+						self.user_input.ip = ip;
+					}
+					EntryInput::BroadcastAddress(broadcast) => {
+						self.user_input.broadcast_address = broadcast;
+					}
+					EntryInput::FirstHost(first) => {
+						self.user_input.first_host = first;
+					}
+					EntryInput::LastHost(last) => {
+						self.user_input.last_host = last;
+					}
+					EntryInput::PossibleHosts(possible) => {
+						self.user_input.possible_hosts = possible;
+					}
+				}
+			}
+			AppMsg::CheckIp => {
+				self.valid = Validator {
+					mask: self.ip.as_ref().map(|ip| &ip.mask).unwrap_or(&"".to_string()) == &self.user_input.mask,
+					network_address: self.ip.as_ref().map(|ip| &ip.network_address).unwrap_or(&"".to_string()) == &self.user_input.ip,
+					broadcast_address: self.ip.as_ref().map(|ip| &ip.broadcast_address).unwrap_or(&"".to_string()) == &self.user_input.broadcast_address,
+					first_host: self.ip.as_ref().map(|ip| &ip.first_host).unwrap_or(&"".to_string()) == &self.user_input.first_host,
+					last_host: self.ip.as_ref().map(|ip| &ip.last_host).unwrap_or(&"".to_string()) == &self.user_input.last_host,
+					possible_hosts: self.ip.as_ref().map(|ip| &ip.possible_hosts).unwrap_or(&0) == &self.user_input.possible_hosts,
+				};
+			}
+		}
+	}
 }
 
 fn ip_calculations() -> IpModel {
-    let mut rng = thread_rng();
-     let octets: [u8; 4] = [
-        rng.gen_range(111..255),
-        rng.gen_range(111..255),
-        rng.gen_range(111..255),
-        rng.gen_range(111..255),
-    ];
-    let subnet_mask: u8 = rng.gen_range(18..28);
-    let prefix = format!("\\{}", subnet_mask);
-    let host_bits = 32-subnet_mask;
-    let ip = format!("{}.{}.{}.{}", octets[0], octets[1], octets[2], octets[3]);
-    let mut binary_mask = "1".repeat(subnet_mask as usize) + &"0".repeat(32-subnet_mask as usize);
-    binary_mask = add_dots(&binary_mask);
-    let binary_address = format!("{:08b}.{:08b}.{:08b}.{:08b}", octets[0], octets[1], octets[2], octets[3]);
+	let mut rng = thread_rng();
+	let octets: [u8; 4] = [
+		rng.gen_range(111..255),
+		rng.gen_range(111..255),
+		rng.gen_range(111..255),
+		rng.gen_range(111..255),
+	];
+	let subnet_mask: u8 = rng.gen_range(18..28);
+	let prefix = format!("\\{}", subnet_mask);
+	let ip = format!("{}.{}.{}.{}", octets[0], octets[1], octets[2], octets[3]);
+	let mut binary_mask = "1".repeat(subnet_mask as usize) + &"0".repeat(32 - subnet_mask as usize);
+	binary_mask = add_dots(&binary_mask);
+	let binary_address = format!("{:08b}.{:08b}.{:08b}.{:08b}", octets[0], octets[1], octets[2], octets[3]);
 
-    // calculate network address
-    let mut network_address = String::new();
-    for (i, c) in binary_address.chars().enumerate() {
-        if c == '.' {
-            network_address.push('.');
-        } else {
-            network_address.push_str(&((c.to_digit(10).unwrap() & binary_mask.chars().nth(i).unwrap().to_digit(10).unwrap()).to_string()));
-        }
-    }
+	// calculate network address
+	let mut network_address = String::new();
+	for (i, c) in binary_address.chars().enumerate() {
+		if c == '.' {
+			network_address.push('.');
+		} else {
+			network_address.push_str(&((c.to_digit(10).unwrap() & binary_mask.chars().nth(i).unwrap().to_digit(10).unwrap()).to_string()));
+		}
+	}
 
-    // calculate broadcast address
-    let mut broadcast_address = String::new();
-    for (i, c) in binary_address.chars().enumerate() {
-        if i >= subnet_mask as usize {
-            if c == '.' {
-                broadcast_address.push('.');
-            } else {
-                broadcast_address.push('1');
-            }
-        } else {
-            broadcast_address.push(c);
-        }
-    }
-    // calculate broadcast address
-    let mut broadcast_address = String::new();
-    for (i, c) in network_address.chars().enumerate() {
-        if i >= (subnet_mask + subnet_mask/8) as usize {
-            if c == '.' {
-                broadcast_address.push('.');
-            } else {
-                broadcast_address.push('1');
-            }
-        } else {
-            broadcast_address.push(c);
-        }
-    }
+	// calculate broadcast address
+	let mut broadcast_address = String::new();
+	for (i, c) in binary_address.chars().enumerate() {
+		if i >= subnet_mask as usize {
+			if c == '.' {
+				broadcast_address.push('.');
+			} else {
+				broadcast_address.push('1');
+			}
+		} else {
+			broadcast_address.push(c);
+		}
+	}
+	// calculate broadcast address
+	let mut broadcast_address = String::new();
+	for (i, c) in network_address.chars().enumerate() {
+		if i >= (subnet_mask + subnet_mask / 8) as usize {
+			if c == '.' {
+				broadcast_address.push('.');
+			} else {
+				broadcast_address.push('1');
+			}
+		} else {
+			broadcast_address.push(c);
+		}
+	}
 
-    // calculate first host address
-    let mut first_host = network_address.clone();
-    let first_host_int = u32::from_str_radix(&first_host.replace(".", ""), 2).unwrap() + 1;
-    first_host = format!("{:032b}", first_host_int);
-    first_host = add_dots(&first_host);
-    // calculate last host address
-    let mut last_host = broadcast_address.clone();
-    let last_host_int = u32::from_str_radix(&last_host.replace(".", ""), 2).unwrap() - 1;
-    last_host = format!("{:032b}", last_host_int);
-    last_host = add_dots(&last_host);
+	// calculate first host address
+	let mut first_host = network_address.clone();
+	let first_host_int = u32::from_str_radix(&first_host.replace(".", ""), 2).unwrap() + 1;
+	first_host = format!("{:032b}", first_host_int);
+	first_host = add_dots(&first_host);
+	// calculate last host address
+	let mut last_host = broadcast_address.clone();
+	let last_host_int = u32::from_str_radix(&last_host.replace(".", ""), 2).unwrap() - 1;
+	last_host = format!("{:032b}", last_host_int);
+	last_host = add_dots(&last_host);
 
-    broadcast_address = binary_dotted_to_decimal(&broadcast_address);
+	broadcast_address = binary_dotted_to_decimal(&broadcast_address);
 
-    first_host = binary_dotted_to_decimal(&first_host);
-    binary_mask = binary_dotted_to_decimal(&binary_mask);
-    last_host = binary_dotted_to_decimal(&last_host);
-    network_address = binary_dotted_to_decimal(&network_address);
-    // calculate number of possible hosts
-    let number_of_hosts = if subnet_mask < 31 { 2u32.pow(32 - subnet_mask as u32) - 2 } else { 0 };
-    let model = IpModel {
-        ip,
-        subnet_mask,
-        prefix,
-        mask: binary_mask,
-        binary_address,
-        network_address,
-        broadcast_address,
-        first_host,
-        last_host,
-        possible_hosts: number_of_hosts,
-    };
-    return model;
+	first_host = binary_dotted_to_decimal(&first_host);
+	binary_mask = binary_dotted_to_decimal(&binary_mask);
+	last_host = binary_dotted_to_decimal(&last_host);
+	network_address = binary_dotted_to_decimal(&network_address);
+	// calculate number of possible hosts
+	let number_of_hosts = if subnet_mask < 31 { 2u32.pow(32 - subnet_mask as u32) - 2 } else { 0 };
+	let model = IpModel {
+		ip,
+		subnet_mask,
+		prefix,
+		mask: binary_mask,
+		binary_address,
+		network_address,
+		broadcast_address,
+		first_host,
+		last_host,
+		possible_hosts: number_of_hosts,
+	};
+	return model;
 }
 
 fn add_dots(binary_string: &str) -> String {
-    let mut result = String::new();
-    for (i, c) in binary_string.chars().enumerate() {
-        if i % 8 == 0 && i != 0 {
-            result.push('.');
-        }
-        result.push(c);
-    }
-    result
+	let mut result = String::new();
+	for (i, c) in binary_string.chars().enumerate() {
+		if i % 8 == 0 && i != 0 {
+			result.push('.');
+		}
+		result.push(c);
+	}
+	result
 }
+
 fn binary_dotted_to_decimal(binary_dotted: &str) -> String {
-    let mut result = String::new();
-    for octet in binary_dotted.split('.') {
-        result.push_str(&u8::from_str_radix(octet, 2).unwrap().to_string());
-        result.push('.');
-    }
-    result.pop();
-    result
+	let mut result = String::new();
+	for octet in binary_dotted.split('.') {
+		result.push_str(&u8::from_str_radix(octet, 2).unwrap().to_string());
+		result.push('.');
+	}
+	result.pop();
+	result
 }
-
-
-
 
 
 fn main() {
-    let app = RelmApp::new("timtom2016.com.IpChecker");
+	let app = RelmApp::new("timtom2016.com.IPChecker");
 
-    relm4_icons::initialize_icons();
-    app.run::<AppModel>(Some(ip_calculations()));
+	relm4_icons::initialize_icons();
+	app.run::<AppModel>(Some(ip_calculations()));
 }
